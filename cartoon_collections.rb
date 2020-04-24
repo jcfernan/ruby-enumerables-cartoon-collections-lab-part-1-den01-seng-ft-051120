8809a3ce56799992c497a22ce3d38aefaccde4ef
Juan Fernandez <jcfernan@live.com>
2020-04-24 02:42:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each
+  array.each_index {|array| print array}
  
   # Use `each_with_index` to enumerate over the provided array
   #
